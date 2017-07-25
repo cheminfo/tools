0eb12a4e433c23ff82c8427cd23b5e4803a1d3a4
Daniel Kostro <kostro.d@gmail.com>
2017-07-25 16:37:27 +0200

    add docs as alias of doc command

--- bin/cheminfo.js
+++ bin/cheminfo.js
@@ -10,6 +10,7 @@ program.version(pkg.version);
 program
     .command('build', 'build a project for the browser')
     .command('doc', 'generate and optionally publish documentation')
+    .alias('docs')
     .command('publish', 'bump and publish a project on npm')
     .command('generate', 'generates a basic project structure');
 
