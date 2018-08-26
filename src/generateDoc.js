006550b557f664c22099baee985ef74c31198ba4
Daniel Kostro <kostro.d@gmail.com>
2018-08-26 11:08:55 +0200

    allow to build docs with typedoc

--- src/generateDoc.js
+++ src/generateDoc.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const execa = require('execa');
+const terminalLink = require('terminal-link');
 const fs = require('mz/fs');
 const inquirer = require('inquirer');
 const path = require('path');
+const { detectTypescript, detectTypedoc } = require('./util');
 
 module.exports = function* generateDoc(publish) {
   const hasDoc = yield fs.exists('docs');
@@ -18,22 +20,48 @@ module.exports = function* generateDoc(publish) {
     })).c;
   }
   if (wantsDoc) {
-    const documentationExecPath = path.resolve(
-      __dirname,
-      '../node_modules/.bin/documentation'
-    );
-    const pkg = require(process.cwd() + '/package.json');
-    const main = pkg.module || pkg.main || '';
-    yield execa(documentationExecPath, [
-      'build',
-      main,
-      '--github',
-      '--output docs',
-      '--format',
-      'html',
-      '--sort-order',
-      'alpha'
-    ]);
+    const isTypescript = yield detectTypescript();
+    if (isTypescript) {
+      const hasTypedocConfig = yield detectTypedoc();
+      const typedocLink = terminalLink('typedoc', 'https://typedoc.org/');
+      console.log('generating docs for typescript project with', typedocLink);
+      const documentationExecPath = path.resolve(
+        __dirname,
+        '../node_modules/.bin/typedoc'
+      );
+      const typedocArgs = ['--out', 'docs', 'src/index.ts'];
+      if (hasTypedocConfig) {
+        typedocArgs.push('--options', 'typedoc.config.js');
+      } else {
+        console.log(
+          'you can customize the output by writing a typedoc.config.js file'
+        );
+      }
+      yield execa(documentationExecPath, typedocArgs);
+    } else {
+      const documentationLibLink = terminalLink(
+        'documentation',
+        'https://github.com/documentationjs/documentation'
+      );
+      console.log('generating js docs with', documentationLibLink);
+
+      const documentationExecPath = path.resolve(
+        __dirname,
+        '../node_modules/.bin/documentation'
+      );
+      const pkg = require(process.cwd() + '/package.json');
+      const main = pkg.module || pkg.main || '';
+      yield execa(documentationExecPath, [
+        'build',
+        main,
+        '--github',
+        '--output docs',
+        '--format',
+        'html',
+        '--sort-order',
+        'alpha'
+      ]);
+    }
     if (publish) {
       yield execa('git', ['add', 'docs']);
       yield execa('git', ['commit', '-m', 'doc: rebuild docs [ci skip]']);
