9743ffd4430a14354db9a8104a9c1104006ddf76
Michaël Zasso <targos@protonmail.com>
2020-02-24 10:36:34 +0100

    chore: update dependencies
    Replace deprecated rollup plugins

--- packages/cheminfo-build/bin/cheminfo-build.js
+++ packages/cheminfo-build/bin/cheminfo-build.js
@@ -7,9 +7,9 @@ const fs = require('fs');
 
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
-const commonjs = require('rollup-plugin-commonjs');
-const json = require('rollup-plugin-json');
-const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+const resolve = require('@rollup/plugin-node-resolve');
 const { terser } = require('rollup-plugin-terser');
 const yargs = require('yargs');
 
